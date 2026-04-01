c316fe2437888198e1f9cde6203cf09b404c4f88
jaswanthkumarck-eng <jaswanthkumar.ck@gmail.com>
2026-04-01 15:48:32 +0530

    UC3

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 abstract class Room {
     private int roomNumber;
     private int beds;
@@ -13,9 +16,9 @@ abstract class Room {
 
     public void displayDetails() {
         System.out.println("Room No: " + roomNumber);
-        System.out.println("Type: " + getType());
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: " + price);
+        System.out.println("Type   : " + getType());
+        System.out.println("Beds   : " + beds);
+        System.out.println("Price  : " + price);
     }
 }
 
@@ -25,7 +28,7 @@ class SingleRoom extends Room {
     }
 
     public String getType() {
-        return "Single Room";
+        return "Single";
     }
 }
 
@@ -35,7 +38,7 @@ class DoubleRoom extends Room {
     }
 
     public String getType() {
-        return "Double Room";
+        return "Double";
     }
 }
 
@@ -45,32 +48,61 @@ class SuiteRoom extends Room {
     }
 
     public String getType() {
-        return "Suite Room";
+        return "Suite";
+    }
+}
+
+// UC3: Centralized Inventory using HashMap
+class RoomInventory {
+    private Map<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
+    }
+
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    public void updateAvailability(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    public void displayInventory() {
+        System.out.println("=== Room Inventory (v3.0) ===");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
     }
 }
 
 public class BookMyStayApp {
 
     public static void main(String[] args) {
+
         Room r1 = new SingleRoom(101);
         Room r2 = new DoubleRoom(102);
         Room r3 = new SuiteRoom(103);
 
-        boolean isSingleAvailable = true;
-        boolean isDoubleAvailable = false;
-        boolean isSuiteAvailable = true;
+        RoomInventory inventory = new RoomInventory();
 
-        System.out.println("=== BookMyStayApp v2.0 ===");
+        System.out.println("=== BookMyStayApp v3.0 ===\n");
 
         r1.displayDetails();
-        System.out.println("Available: " + isSingleAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Single"));
         System.out.println();
 
         r2.displayDetails();
-        System.out.println("Available: " + isDoubleAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Double"));
         System.out.println();
 
         r3.displayDetails();
-        System.out.println("Available: " + isSuiteAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Suite"));
+        System.out.println();
+
+        inventory.displayInventory();
     }
 }
